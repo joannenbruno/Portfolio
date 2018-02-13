5113d822052dc0db706a471c94f5f2df6ec2752e
Joanne Bruno <jnbruno2015@gmail.com>
2018-02-13 09:05:11 -0600

    Updated project content and images

--- scripts/app.js
+++ scripts/app.js
@@ -1,38 +1,43 @@
 // wrap in an IIFE to remove variables from global scope
-(function() {
+(function () {
 	'use strict';
 
 	angular.module('folioGenericApp', ['ui.router', 'responsive-images'])
-		.config(function($stateProvider, $urlRouterProvider, $compileProvider, $locationProvider) {
+		.config(function ($stateProvider, $urlRouterProvider, $compileProvider, $locationProvider) {
 
 			$compileProvider.debugInfoEnabled(false);
 
 			$stateProvider
-			.state('developer', {
-				url: '/developer',
-				templateUrl: 'templates/main/developer.html',
-				controller: 'DeveloperController'
-			})
-			.state('artist', {
-				url: '/artist',
-				templateUrl: 'templates/main/artist.html',
-				controller: 'ArtistController'
-			})
-			.state('demo-photoshoot', {
-				url: '/demo-photoshoot',
-				templateUrl: 'templates/photoshoots/demo-photoshoot.html',
-				controller: 'ArtistController'
-			})
-			.state('demo-project', {
-				url: '/demo-project',
-				templateUrl: 'templates/projects/demo-project.html',
-				controller: 'DeveloperController'
-			})
-			.state('demo-project-two', {
-				url: '/demo-project-two',
-				templateUrl: 'templates/projects/demo-project-two.html',
-				controller: 'DeveloperController'
-			});
+				.state('developer', {
+					url: '/developer',
+					templateUrl: 'templates/main/developer.html',
+					controller: 'DeveloperController'
+				})
+				.state('artist', {
+					url: '/artist',
+					templateUrl: 'templates/main/artist.html',
+					controller: 'ArtistController'
+				})
+				.state('demo-photoshoot', {
+					url: '/demo-photoshoot',
+					templateUrl: 'templates/photoshoots/demo-photoshoot.html',
+					controller: 'ArtistController'
+				})
+				.state('demo-project', {
+					url: '/demo-project',
+					templateUrl: 'templates/projects/demo-project.html',
+					controller: 'DeveloperController'
+				})
+				.state('demo-project-two', {
+					url: '/demo-project-two',
+					templateUrl: 'templates/projects/demo-project-two.html',
+					controller: 'DeveloperController'
+				})
+				.state('keep-my-casa', {
+					url: '/keep-my-casa',
+					templateUrl: 'templates/projects/project-keep-my-casa.html',
+					controller: 'DeveloperController'
+				});
 
 			$urlRouterProvider.otherwise('/developer');
 		});
